e4d6f30fe1b3706894dc540e95f7e93adee49a8a
hansonboy <jianwei2013@126.com>
2016-03-27 22:23:46 +0800

    当出现popver后，仍然可以点击后面的其他控件

--- UIPoperViewController/ViewController.m
+++ UIPoperViewController/ViewController.m
@@ -11,7 +11,8 @@
 const NSString *JWPopoverShouldDismiss =  @"JWPopoverShouldDismiss";
 @interface ViewController ()<UIPopoverControllerDelegate,JWPopverTableViewControllerDelegate>
 
-@property (strong,nonatomic)UIPopoverController *popverController;
+@property (weak, nonatomic) IBOutlet UIButton *otherButton;
+@property (strong,nonatomic) UIPopoverController *popverController;
 @property (weak, nonatomic) IBOutlet UIBarButtonItem *item;
 @property (weak, nonatomic) IBOutlet UIButton *button;
 @end
@@ -29,6 +30,8 @@ const NSString *JWPopoverShouldDismiss =  @"JWPopoverShouldDismiss";
          controller.popover = _popverController;
         _popverController.delegate = self;
         [[NSNotificationCenter defaultCenter]addObserver:_popverController selector:@selector(dismissPopoverAnimated:) name:JWPopoverShouldDismiss object:nil];
+        
+        _popverController.passthroughViews = @[self.otherButton ];
     }
     return _popverController;
 
